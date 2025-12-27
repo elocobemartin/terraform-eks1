48e4d2b53d4284b41de94edb620776cf9ed87309
Martin Elocobe <elocobemart@gmail.com>
2025-12-27 03:35:00 +0100

    update on versions

--- versions.tf
+++ versions.tf
@@ -1,30 +1,25 @@
 
 terraform {
-  required_version = ">= 0.12"
   required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.1.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">=2.7.1"
-    }
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.68.0"
+      version = ">= 5.10"
+      region  = "us-east-1"
     }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7.1"
+    }
+
     local = {
       source  = "hashicorp/local"
       version = "~> 2.1.0"
     }
+
     null = {
       source  = "hashicorp/null"
       version = "~> 3.1.0"
-    }
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.2.0"
     }
   }
 }
